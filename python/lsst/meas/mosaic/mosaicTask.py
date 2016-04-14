a63448b98217bb8099a89175cab01fe3a62ecbed
John Swinbank <swinbank@princeton.edu>
2016-04-14 10:31:55 -0400

    Update to match recent Butler API changes.
    This is necessary following the merger of DM-4683.

--- python/lsst/meas/mosaic/mosaicTask.py
+++ python/lsst/meas/mosaic/mosaicTask.py
@@ -49,7 +49,7 @@ class MosaicRunner(pipeBase.TaskRunner):
         for ref in parsedCmd.id.refList:
             refListDict.setdefault(ref.dataId["tract"], []).append(ref)
         # we call run() once with each tract
-        return [(parsedCmd.butler.mapper.getCameraName(),
+        return [(parsedCmd.camera,
                  parsedCmd.butler,
                  tract,
                  refListDict[tract],
