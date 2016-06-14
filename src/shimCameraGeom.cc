40534c516e70646ac31261d95487337f97eb7a69
Lauren MacArthur <lauren@astro.princeton.edu>
2016-06-14 18:28:16 -0400

    Remove commented out code in shimCameraGeom.cc

--- src/shimCameraGeom.cc
+++ src/shimCameraGeom.cc
@@ -6,7 +6,6 @@ namespace meas {
 namespace mosaic {
 
 int getNQuarter(CONST_PTR(afw::cameraGeom::Detector) det) {
-//    return det->getOrientation().getNQuarter() % 4;
     return det->getOrientation().getNQuarter();
 }
 
@@ -49,33 +48,16 @@ afw::geom::Point2D getCenterInDetectorPixels(CONST_PTR(afw::cameraGeom::Detector
 }
 
 int getWidth(CONST_PTR(afw::cameraGeom::Detector) det) {
-//    if ((getNQuarter(det) % 2) != 0) {
-//        return det->getBBox().getHeight();
-//    } else {
     return det->getBBox().getWidth();
-//    }
 }
 
 int getHeight(CONST_PTR(afw::cameraGeom::Detector) det) {
-//    if ((getNQuarter(det) % 2) != 0) {
-//        return det->getBBox().getWidth();
-//    } else {
     return det->getBBox().getHeight();
-//    }
 }
 
 afw::geom::Point2D detPxToFpPx(CONST_PTR(afw::cameraGeom::Detector) det, afw::geom::Point2D const detPt) {
     afw::geom::Point2D point;
-//    if (getNQuarter(det) == 3) {
-//        point = afw::geom::Point2D(det->getBBox().getWidth() - detPt.getY() - 1, detPt.getX());
-//    } else if (getNQuarter(det) == 2) {
-//        point = afw::geom::Point2D(det->getBBox().getWidth() - detPt.getX() - 1,
-//                                   det->getBBox().getHeight() - detPt.getY() - 1);
-//    } else if (getNQuarter(det) == 1) {
-//        point = afw::geom::Point2D(detPt.getY(), det->getBBox().getHeight() - detPt.getX() - 1);
-//    } else {
     point = detPt;
-//    }
     auto scaling = makeScalingMmToPx(det->getPixelSize());
     return scaling(det->transform(det->makeCameraPoint(point, afw::cameraGeom::PIXELS),
                                   afw::cameraGeom::FOCAL_PLANE).getPoint());
