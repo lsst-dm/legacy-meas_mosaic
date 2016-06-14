1fb155ae4e47c260b213a14f11450fe5f5008e4c
Lauren MacArthur <lauren@astro.princeton.edu>
2016-06-14 18:26:06 -0400

    Update and reactivate testCameraGeomShim

--- tests/testCameraGeomShim.py
+++ tests/testCameraGeomShim.py
@@ -38,7 +38,7 @@ except ImportError:
 class ShimCameraGeomTestCase(utilsTests.TestCase):
     def setUp(self):
         self.camera = obsHsc.HscMapper(root=".").camera
-        self.ccds = [self.camera[99],   # 0_53; no rotation
+        self.ccds = [self.camera[49],   # 0_12; no rotation
                      self.camera[102],  # 0_35; 1 quarter rotation
                      self.camera[29],   # 1_03; 2 quarter rotations
                      self.camera[100]]  # 0_31; 3 quarter rotations
@@ -54,8 +54,8 @@ class ShimCameraGeomTestCase(utilsTests.TestCase):
             self.assertEqual(measMosaic.getNQuarter(ccd), quarter)
 
     def testGetYaw(self):
-        # HSC camGeom: ccd.getOrientation().getYaw()
-        known_results = [-0.0439981, 0.0296679, -0.0075553, -0.0112589]
+        # HSC camGeom: ccd.getOrientation().getYaw().asDegrees()
+        known_results = [ 0.0073295, 0.0296679, -0.0075553, -0.0112589]
         for ccd, yaw in zip(self.ccds, known_results):
             self.assertAlmostEqual(measMosaic.getYaw(ccd).asDegrees(), yaw)
 
@@ -69,7 +69,7 @@ class ShimCameraGeomTestCase(utilsTests.TestCase):
 
     def testGetCenterInFpPixels(self):
         # HSC camGeom: ccd.getCenter().getPixels(ccd.getPixelSize())
-        known_results = [afwGeom.Point2D(-14862.74, 7030.61), afwGeom.Point2D(-9591.84, -14290.21),
+        known_results = [afwGeom.Point2D(0.28, -1925.76), afwGeom.Point2D(-9591.84, -14290.21),
                          afwGeom.Point2D(6368.58, 15753.83), afwGeom.Point2D(9588.26, -14289.16)]
         for ccd, center in zip(self.ccds, known_results):
             self.assertAlmostEqual(measMosaic.getCenterInFpPixels(ccd).getX(), center.getX())
@@ -82,39 +82,30 @@ class ShimCameraGeomTestCase(utilsTests.TestCase):
         for ccd, center in zip(self.ccds, known_results):
             self.assertEqual(measMosaic.getCenterInDetectorPixels(ccd), center)
 
-    def testGetWidth(self):
-        # HSC camGeom: ccd.getWidth()
-        known_results = [2048, 4176, 2048, 4176]
-        for ccd, width in zip(self.ccds, known_results):
-            self.assertEqual(measMosaic.getWidth(ccd), width)
-
-    def testGetHeight(self):
-        # HSC camGeom: ccd.getHeight()
-        known_results = [4176, 2048, 4176, 2048]
-        for ccd, width in zip(self.ccds, known_results):
-            self.assertEqual(measMosaic.getHeight(ccd), width)
-
-    def testDetPxToFpPx(self):
+    def testDetPxToFpPxRot(self):
         # HSC camGeom: ccd.getPositionFromPixel(point).getPixels(ccd.getPixelSize())
-        points = [afwGeom.Point2D(0, 0), afwGeom.Point2D(100, 100)]
-        known_results = [(afwGeom.Point2D(-15886.24, 4943.11), afwGeom.Point2D(-15786.1632383, 5043.03317935)),
-                         (afwGeom.Point2D(-11679.34, -15313.71), afwGeom.Point2D(-11579.3917937, -15213.6582332)),
-                         (afwGeom.Point2D(5345.08, 13666.33), afwGeom.Point2D(5445.09318562, 13766.3168126)),
-                         (afwGeom.Point2D(7500.76, -15312.66), afwGeom.Point2D(7600.77964856, -15212.6796524))]
+        points = [afwGeom.Point2D(0.0, 0.0), afwGeom.Point2D(100.0, 100.0)]
+        known_results = [(afwGeom.Point2D(-1023.22, -4013.26),
+                          afwGeom.Point2D(-923.232793209, -3913.24720843)),
+                         (afwGeom.Point2D(-11679.34, -15313.71),
+                          afwGeom.Point2D(-11579.3917937, -15213.6582332)),
+                         (afwGeom.Point2D(5345.08, 13666.33),
+                          afwGeom.Point2D(5445.09318562, 13766.3168126)),
+                         (afwGeom.Point2D(7500.76, -15312.66),
+                          afwGeom.Point2D(7600.77964856, -15212.6796524))]
         for ccd, pts in zip(self.ccds, known_results):
             for point, pt in zip(points, pts):
                 pt.getX()
-                self.assertAlmostEqual(measMosaic.detPxToFpPx(ccd, point).getX()/pt.getX(), 1, 3)
-                self.assertAlmostEqual(measMosaic.detPxToFpPx(ccd, point).getY()/pt.getY(), 1, 3)
-
+                self.assertAlmostEqual(measMosaic.detPxToFpPxRot(ccd, point).getX(), pt.getX(), 5)
+                self.assertAlmostEqual(measMosaic.detPxToFpPxRot(ccd, point).getY(), pt.getY(), 5)
 
 def suite():
     """Returns a suite containing all the test cases in this module."""
     utilsTests.init()
 
     suites = []
-    # suites += unittest.makeSuite(ShimCameraGeomTestCase)
-    # suites += unittest.makeSuite(utilsTests.MemoryTestCase)
+    suites += unittest.makeSuite(ShimCameraGeomTestCase)
+    suites += unittest.makeSuite(utilsTests.MemoryTestCase)
 
     return unittest.TestSuite(suites)
 
